d25c723095591b025f53a985cc83a58e4af0f36a
Matheus Rocha <matheusrochaalmeida7@gmail.com>
2025-10-22 23:16:26 -0300

    ajuste dark/light

---
+++ Assets/js/engine.js
@@ -0,0 +1,14 @@
+
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
+
+toggleTheme.addEventListener("click", ()=>{
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") 
+    : rootHtml.setAttribute("data-theme", "dark");
+
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+
+})
